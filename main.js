056175f4d0e48b9386cc5f179434bc4af6315f7d
Ankit sharma <155532533+demon07101845@users.noreply.github.com>
2024-12-02 09:59:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ var typed = new Typed('.text', {
   backDelay: 1000,
   loop: true,
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('#menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+});
+
+// document.querySelectorAll('.navbar a').forEach((link) => {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const targetId = this.getAttribute('href').substring(1);
+//     const targetElement = document.getElementById(targetId);
+
+//     if (targetElement) {
+//       window.scrollTo({
+//         top: targetElement.offsetTop,
+//         behavior: 'smooth',
+//       });
+//     }
+//   });
+// });
+
